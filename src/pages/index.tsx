03aa7d85756ae96b8ac20e5177b59b4109176899
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-09-18 23:06:56 -0300

    feat: new home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import TypewriterComponent from 'typewriter-effect';
+import cx from 'classnames';
 
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
@@ -17,26 +18,77 @@ const Home: NextPage = () => {
       <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          Welcome to <strong>my Website!</strong>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <h3 className={styles.subTitle}>
+          <span>Vamos fazer a diferença juntos?</span>
+          <br />
+          <code className={styles.code}>bora/codar.ts</code>
+        </h3>
+
+        <h2 className={cx(styles.title, styles.secondary)}>
+          Atualmente sou um <strong>Desenvolvedor Frontend Junior</strong> e um{' '}
+          <strong>Estudante Entusiasmado</strong>
+        </h2>
+
+        <p className={styles.typewriterSection}>
+          <TypewriterComponent
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('Junior Frontend Developer')
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString('Web Development')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Fullstack Enthusiastic')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Student')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Technology Lover')
+                .pauseFor(500)
+                .deleteAll()
+                .start();
+            }}
+            options={{
+              delay: 80,
+              deleteSpeed: 25,
+              loop: true,
+            }}
+          />
         </p>
 
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <button className={styles.contactMe}>contate-me</button>
 
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <div className={styles.grid}>
+          <a
+            href='https://www.linkedin.com/in/viniciusalvesdefaria/'
+            className={styles.card}
+            target='_blank'
+            rel='noreferrer'>
+            <h2>Conecte-se pelo Linkedin &rarr;</h2>
+            <p>
+              Você pode conectar-se comigo pelo Linkedin para então fazermos
+              contato!
+            </p>
           </a>
 
           <a
+            href='https://github.com/ViniciusResende'
+            className={styles.card}
+            target='_blank'
+            rel='noreferrer'>
+            <h2>Encontre-me no GitHub &rarr;</h2>
+            <p>
+              Você pode me achar pelo GitHub onde estou sempre colocando algumas
+              novidades.
+            </p>
+          </a>
+
+          {/* <a
             href='https://github.com/vercel/next.js/tree/master/examples'
             className={styles.card}>
             <h2>Examples &rarr;</h2>
@@ -50,7 +102,7 @@ const Home: NextPage = () => {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
