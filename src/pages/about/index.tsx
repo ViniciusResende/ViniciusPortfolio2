a0dfe22dc772c8bd3619096b2e1f26b6d518a541
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-09-19 17:23:03 -0300

    feat: about page adjustment

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
 
 import { Header } from '../../components/Header';
@@ -9,6 +10,9 @@ import styles from './About.module.scss';
 const About = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>About | Vinícius Alves</title>
+      </Head>
       <Header />
       <main className={styles.content}>
         <aside className={styles.pictureContainer}>
@@ -38,11 +42,11 @@ const About = () => {
             Olá, me chamo Vinícius Alves de Faria. Tenho inglês avançado, nível
             B2, pela formação na UpTime, Técnico de Informática formado pela
             Universidade Federal de Viçosa, e estudante de Sistemas de
-            Informação pela Pontifícia Universidade Católica de Minas Gerais,
-            recebi prêmios de honra ao mérito por aproveitamento durante o meu
-            curso técnico e ensino médio pela UFV, prêmio de primeiro lugar na
-            feira FECITEC que ocorre na UFV - Florestal, nesta feira apresentei
-            um projeto na área multidisciplinar (com presença de programação de
+            Informação pela Universidade Federal de Minas Gerais, recebi prêmios
+            de honra ao mérito por aproveitamento durante o meu curso técnico e
+            ensino médio pela UFV, prêmio de primeiro lugar na feira FECITEC que
+            ocorre na UFV - Florestal, nesta feira apresentei um projeto na área
+            multidisciplinar (com presença de programação de
             microcontroladores), esse trabalho foi indicado à apresentação na
             FEBRACE de São Paulo e na London International Youth Science Forum
             (LIYSF) em Londres, infelizmente, ambos os eventos foram cancelados
@@ -50,9 +54,11 @@ const About = () => {
             Logística, posteriormente como desenvolvedor VBA e WEB pela mesma
             companhia. Realizei projetos no que tange ao desenvolvimento de
             aplicativos utilizando as tecnologias WEB, principalmente, React
-            Native, ReactJS, NodeJS dentre outras tecnologias, esses projetos
-            foram desenvolvidos perante a orientação de cursos gratuitos
-            disponibilizados na internet e/ou professores da UFV.
+            Native, ReactJS, NodeJS, Elixir dentre outras tecnologias, esses
+            projetos foram desenvolvidos perante a orientação de cursos
+            gratuitos disponibilizados na internet e/ou professores da UFV.
+            Atualmente, sou desenvolvedor frontend na Americanas SA e estou cada
+            vez mais desenvolvendo minhas habilidades.
           </p>
         </aside>
       </main>
