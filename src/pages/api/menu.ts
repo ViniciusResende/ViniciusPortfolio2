642567d4d4b77cd753ce6c64798e768311ca7026
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-10-01 22:02:44 -0300

    feat: api data folder restructure

--- src/pages/api/menu.ts
+++ src/pages/api/menu.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import MenuItems from './defaultData/menuItems.json';
+import MenuItems from './Data/menuItems.json';
 
 type MenuItemData = {
   name: string;
