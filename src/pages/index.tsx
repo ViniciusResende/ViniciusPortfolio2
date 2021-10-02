945a830d3e04c61c2b74aefcfb7785c68b05d747
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-10-02 10:15:56 -0300

    feat: Home page layout component implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,213 +4,208 @@ import TypewriterComponent from 'typewriter-effect';
 import Particles from 'react-tsparticles';
 import cx from 'classnames';
 
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
+import { Layout } from '../components/Layout';
 
 import styles from '../styles/Home/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <section className={styles.container}>
-      <Head>
-        <title>Home | Vinícius Alves</title>
-      </Head>
-      <Header />
-      <section className={styles.particlesContainer}>
-        <Particles
-          params={{
-            background: {
-              position: '50% 50%',
-              repeat: 'no-repeat',
-              size: 'cover',
-            },
-            fullScreen: {
-              enable: true,
-              zIndex: 1,
-            },
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: 'push',
+    <Layout title='Home'>
+      <div className={styles.content}>
+        <section className={styles.particlesContainer}>
+          <Particles
+            params={{
+              background: {
+                position: '50% 50%',
+                repeat: 'no-repeat',
+                size: 'cover',
+              },
+              fullScreen: {
+                enable: true,
+                zIndex: 1,
+              },
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: 'push',
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: 'repulse',
+                    parallax: {
+                      force: 60,
+                    },
+                  },
                 },
-                onHover: {
-                  enable: true,
-                  mode: 'repulse',
-                  parallax: {
-                    force: 60,
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  grab: {
+                    distance: 400,
                   },
                 },
               },
-              modes: {
-                bubble: {
-                  distance: 400,
-                  duration: 2,
-                  opacity: 0.8,
-                  size: 40,
-                },
-                grab: {
-                  distance: 400,
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: '#3178c6',
-              },
-              links: {
+              particles: {
                 color: {
-                  value: '#ffffff',
+                  value: '#3178c6',
                 },
-                distance: 150,
-                enable: true,
-                opacity: 0.4,
-              },
-              move: {
-                attract: {
-                  rotate: {
-                    x: 600,
-                    y: 1200,
+                links: {
+                  color: {
+                    value: '#ffffff',
                   },
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.4,
                 },
-                enable: true,
-              },
-              number: {
-                density: {
+                move: {
+                  attract: {
+                    rotate: {
+                      x: 600,
+                      y: 1200,
+                    },
+                  },
                   enable: true,
                 },
-                value: 80,
-              },
-              opacity: {
-                value: {
-                  min: 0.1,
-                  max: 0.5,
+                number: {
+                  density: {
+                    enable: true,
+                  },
+                  value: 80,
                 },
-                animation: {
-                  enable: true,
-                  speed: 1,
-                  minimumValue: 0.1,
-                },
-              },
-              shape: {
-                type: 'circle',
-              },
-              size: {
-                value: 3,
-                animation: {
-                  speed: 17,
-                  minimumValue: 10,
-                },
-              },
-              stroke: {
-                width: 1,
-                color: {
-                  value: '#ffffff',
+                opacity: {
+                  value: {
+                    min: 0.1,
+                    max: 0.5,
+                  },
                   animation: {
-                    h: {
-                      count: 0,
-                      enable: false,
-                      offset: 0,
-                      speed: 1,
-                      sync: true,
-                    },
-                    s: {
-                      count: 0,
-                      enable: false,
-                      offset: 0,
-                      speed: 1,
-                      sync: true,
-                    },
-                    l: {
-                      count: 0,
-                      enable: false,
-                      offset: 0,
-                      speed: 1,
-                      sync: true,
+                    enable: true,
+                    speed: 1,
+                    minimumValue: 0.1,
+                  },
+                },
+                shape: {
+                  type: 'circle',
+                },
+                size: {
+                  value: 3,
+                  animation: {
+                    speed: 17,
+                    minimumValue: 10,
+                  },
+                },
+                stroke: {
+                  width: 1,
+                  color: {
+                    value: '#ffffff',
+                    animation: {
+                      h: {
+                        count: 0,
+                        enable: false,
+                        offset: 0,
+                        speed: 1,
+                        sync: true,
+                      },
+                      s: {
+                        count: 0,
+                        enable: false,
+                        offset: 0,
+                        speed: 1,
+                        sync: true,
+                      },
+                      l: {
+                        count: 0,
+                        enable: false,
+                        offset: 0,
+                        speed: 1,
+                        sync: true,
+                      },
                     },
                   },
                 },
               },
-            },
-          }}
-        />
-      </section>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <strong>my Website!</strong>
-        </h1>
-
-        <h3 className={styles.subTitle}>
-          <span>Vamos fazer a diferença juntos?</span>
-          <br />
-          <code className={styles.code}>bora/codar.ts</code>
-        </h3>
-
-        <h2 className={cx(styles.title, styles.secondary)}>
-          Atualmente sou um <strong>Desenvolvedor Frontend Junior</strong> e um{' '}
-          <strong>Estudante Entusiasmado</strong>
-        </h2>
-
-        <p className={styles.typewriterSection}>
-          <TypewriterComponent
-            onInit={(typewriter) => {
-              typewriter
-                .typeString('Junior Frontend Developer')
-                .pauseFor(2500)
-                .deleteAll()
-                .typeString('Web Development')
-                .pauseFor(500)
-                .deleteAll()
-                .typeString('Fullstack Enthusiastic')
-                .pauseFor(500)
-                .deleteAll()
-                .typeString('Student')
-                .pauseFor(500)
-                .deleteAll()
-                .typeString('Technology Lover')
-                .pauseFor(500)
-                .deleteAll()
-                .start();
-            }}
-            options={{
-              delay: 80,
-              deleteSpeed: 25,
-              loop: true,
             }}
           />
-        </p>
-
-        <button className={styles.contactMe}>contate-me</button>
-
-        <section className={styles.gridCards}>
-          <a
-            href='https://www.linkedin.com/in/viniciusalvesdefaria/'
-            className={styles.card}
-            target='_blank'
-            rel='noreferrer'>
-            <h2>Conecte-se pelo Linkedin &rarr;</h2>
-            <p>
-              Você pode conectar-se comigo pelo Linkedin para então fazermos
-              contato!
-            </p>
-          </a>
-
-          <a
-            href='https://github.com/ViniciusResende'
-            className={styles.card}
-            target='_blank'
-            rel='noreferrer'>
-            <h2>Encontre-me no GitHub &rarr;</h2>
-            <p>
-              Você pode me achar pelo GitHub onde estou sempre colocando algumas
-              novidades.
-            </p>
-          </a>
         </section>
-      </main>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <strong>my Website!</strong>
+          </h1>
 
-      <Footer />
-    </section>
+          <h3 className={styles.subTitle}>
+            <span>Vamos fazer a diferença juntos?</span>
+            <br />
+            <code className={styles.code}>bora/codar.ts</code>
+          </h3>
+
+          <h2 className={cx(styles.title, styles.secondary)}>
+            Atualmente sou um <strong>Desenvolvedor Frontend Junior</strong> e
+            um <strong>Estudante Entusiasmado</strong>
+          </h2>
+
+          <p className={styles.typewriterSection}>
+            <TypewriterComponent
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('Junior Frontend Developer')
+                  .pauseFor(2500)
+                  .deleteAll()
+                  .typeString('Web Development')
+                  .pauseFor(500)
+                  .deleteAll()
+                  .typeString('Fullstack Enthusiastic')
+                  .pauseFor(500)
+                  .deleteAll()
+                  .typeString('Student')
+                  .pauseFor(500)
+                  .deleteAll()
+                  .typeString('Technology Lover')
+                  .pauseFor(500)
+                  .deleteAll()
+                  .start();
+              }}
+              options={{
+                delay: 80,
+                deleteSpeed: 25,
+                loop: true,
+              }}
+            />
+          </p>
+
+          <button className={styles.contactMe}>contate-me</button>
+
+          <section className={styles.gridCards}>
+            <a
+              href='https://www.linkedin.com/in/viniciusalvesdefaria/'
+              className={styles.card}
+              target='_blank'
+              rel='noreferrer'>
+              <h2>Conecte-se pelo Linkedin &rarr;</h2>
+              <p>
+                Você pode conectar-se comigo pelo Linkedin para então fazermos
+                contato!
+              </p>
+            </a>
+
+            <a
+              href='https://github.com/ViniciusResende'
+              className={styles.card}
+              target='_blank'
+              rel='noreferrer'>
+              <h2>Encontre-me no GitHub &rarr;</h2>
+              <p>
+                Você pode me achar pelo GitHub onde estou sempre colocando
+                algumas novidades.
+              </p>
+            </a>
+          </section>
+        </main>
+      </div>
+    </Layout>
   );
 };
 
