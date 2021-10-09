2b230578a4c1082544abfccf50f6eb0c68071e4e
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-10-09 14:29:51 -0300

    feat: new brands and utils svg icons

--- public/svg/index.ts
+++ public/svg/index.ts
@@ -1,5 +1,7 @@
 import BarsIcon from './bars-solid.svg';
 import ArrowIcon from './arrow.svg';
+import TriangleIcon from './triangle-left.svg';
+import CloseIcon from './times-circle.svg';
 import LinkedinIcon from './linkedin.svg';
 import GitHubIcon from './github.svg';
 import InstagramIcon from './instagram.svg';
@@ -9,16 +11,27 @@ import SassIcon from './sass.svg';
 import CypressIcon from './cypress.svg';
 import AwsIcon from './aws.svg';
 import TypeScriptLogo from './typescript.svg';
+import JavaScriptLogo from './javascript.svg';
 import NpmIcon from './npm.svg';
 import GoogleAnalyticsIcon from './analytics.svg';
 import WebpackIcon from './webpaack.svg';
 import GitIcon from './git.svg';
 import NodeJsIcon from './nodejs.svg';
 import KubernetsIcon from './kubernets.svg';
+import SqliteIcon from './sqlite.svg';
+import MongoIcon from './mongodb.svg';
+import ExpoIcon from './expo.svg';
+import FirebaseIcon from './firebase.svg';
+import GraphQLIcon from './graphql.svg';
+import ApolloGraphQLIcon from './apollographql.svg';
+import StyledComponentsIcon from './styled-components.svg';
+import ExpressIcon from './express.svg';
 
 export {
   BarsIcon,
   ArrowIcon,
+  TriangleIcon,
+  CloseIcon,
   LinkedinIcon,
   GitHubIcon,
   InstagramIcon,
@@ -28,10 +41,19 @@ export {
   CypressIcon,
   AwsIcon,
   TypeScriptLogo,
+  JavaScriptLogo,
   NpmIcon,
   GoogleAnalyticsIcon,
   WebpackIcon,
   GitIcon,
   NodeJsIcon,
   KubernetsIcon,
+  SqliteIcon,
+  MongoIcon,
+  ExpoIcon,
+  FirebaseIcon,
+  GraphQLIcon,
+  ApolloGraphQLIcon,
+  StyledComponentsIcon,
+  ExpressIcon,
 };
