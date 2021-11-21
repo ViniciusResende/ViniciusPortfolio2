840cc374afc99a1ad868f27f0432f88cbd5ad9a4
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-11-21 14:52:43 -0300

    feat: added toats default texts do contact form send

---
+++ src/config/ToastTexts.ts
@@ -0,0 +1,4 @@
+export const send_email_fullfilled =
+  'O formulário foi enviado com sucesso, em breve entrarei em contato. Obrigado pela paciênca.';
+export const send_email_rejected =
+  'Alguma coisa deu errado. Por favor, tente novamente em alguns instantes ou entre em contato por outro meio.';
