c46ddbf1afaced7308e400f785ad18f4f31286fe
Vinicius Alves <viniciusfariaresende@gmail.com>
2021-11-21 14:51:48 -0300

    feat: added email form validator

---
+++ src/helpers/validations.ts
@@ -0,0 +1,6 @@
+export const email = (value: string) => {
+  if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return 'Endereço de email inválido';
+  }
+  return undefined;
+};
